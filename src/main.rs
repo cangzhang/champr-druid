1bb20678a554db27275327d51184c9582d641588
al <cangzan@gmail.com>
2022-01-08 11:08:51 +0800

    change: filter sources.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use druid::im::Vector;
 use druid::widget::{
-    Checkbox, Flex, Label, LensWrap, List, MainAxisAlignment, Padding, Parse, Scroll, Stepper,
-    Switch, TextBox, WidgetExt,
+    Checkbox, Controller, Flex, Label, LensWrap, List, MainAxisAlignment, Padding, Parse, Scroll,
+    Stepper, Switch, TextBox, WidgetExt,
 };
-use druid::{AppLauncher, Data, Lens, LensExt, LocalizedString, Widget, WindowDesc};
+use druid::{
+    AppDelegate, AppLauncher, Data, Handled, Lens, LensExt, LocalizedString, Selector, Widget,
+    WindowDesc,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Data, Lens, Debug)]
@@ -11,8 +14,27 @@ struct AppState {
     pub checked: bool,
     pub on: bool,
     pub stepper_value: f64,
-    pub sources: Vector<SourceItem>,
     pub selected: Vector<String>,
+    pub search: String,
+    pub sources: Vector<SourceItem>,
+    pub filtered_sources: Vector<SourceItem>,
+}
+
+impl AppState {
+    fn filter(&mut self) -> &mut Self {
+        if self.search.chars().count() == 0 {
+            self.filtered_sources = self.sources.clone();
+        } else {
+            self.filtered_sources = self
+                .sources
+                .clone()
+                .into_iter()
+                .filter(|s| s.text.contains(&self.search))
+                .collect();
+        }
+
+        self
+    }
 }
 
 #[derive(Clone, Data, Lens, Serialize, Deserialize, Debug)]
@@ -20,6 +42,48 @@ struct SourceItem {
     pub checked: bool,
     pub text: String,
     pub value: String,
+}
+
+#[derive(Debug)]
+struct UpdateCallback();
+
+impl Controller<String, TextBox<String>> for UpdateCallback {
+    fn update(
+        &mut self,
+        child: &mut TextBox<String>,
+        ctx: &mut druid::UpdateCtx,
+        old_data: &String,
+        data: &String,
+        env: &druid::Env,
+    ) {
+        if old_data != data {
+            ctx.submit_command(FILTER);
+        }
+
+        child.update(ctx, old_data, data, env)
+    }
+}
+
+pub struct Delegate;
+
+const FILTER: Selector = Selector::new("source.filter");
+
+impl AppDelegate<AppState> for Delegate {
+    fn command(
+        &mut self,
+        ctx: &mut druid::DelegateCtx,
+        target: druid::Target,
+        cmd: &druid::Command,
+        data: &mut AppState,
+        env: &druid::Env,
+    ) -> druid::Handled {
+        if cmd.is(FILTER) {
+            data.filter();
+            Handled::Yes
+        } else {
+            Handled::No
+        }
+    }
 }
 
 fn build_widget() -> impl Widget<AppState> {
@@ -65,15 +129,27 @@ fn build_widget() -> impl Widget<AppState> {
     col.add_child(Padding::new(5.0, row));
     col.add_child(Padding::new(5.0, textbox_row));
     col.add_child(Padding::new(5.0, label_row));
+
+    let search_box = TextBox::new()
+        .with_placeholder("搜索")
+        .controller(UpdateCallback())
+        .lens(AppState::search);
+    let mut search_col = Flex::row().with_child(Padding::new(5.0, search_box));
+    search_col.set_main_axis_alignment(MainAxisAlignment::Center);
+    search_col.set_must_fill_main_axis(true);
+    col.add_flex_child(search_col, 1.0);
+
     col.add_flex_child(build_list(), 1.0);
     col.center()
 }
 
 fn build_list() -> impl Widget<AppState> {
+    let list = List::new(build_item).lens(AppState::filtered_sources);
+
     Scroll::new(
         Flex::column()
             .must_fill_main_axis(true)
-            .with_child(List::new(build_item).lens(AppState::sources))
+            .with_child(list)
             .with_default_spacer(),
     )
     .vertical()
@@ -94,7 +170,7 @@ pub fn main() {
         .title(LocalizedString::new("switch-demo-window-title").with_placeholder("Switch Demo"));
 
     let window = window.window_size((320., 540.));
-    
+
     let mut selected = vec![];
     selected.push(String::from("source-1"));
     selected.push(String::from("source-2"));
@@ -113,12 +189,15 @@ pub fn main() {
 
     AppLauncher::with_window(window)
         .use_simple_logger()
+        .delegate(Delegate {})
         .launch(AppState {
             checked: false,
             on: true,
             stepper_value: 1.0,
+            search: String::new(),
+            selected: Vector::from(selected),
             sources: Vector::from(sources),
-            selected: Vector::from(selected)
+            filtered_sources: Vector::new(),
         })
         .expect("launch failed");
 }
